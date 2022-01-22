548a0d90e77ac9049cbe542064eda48b6917d139
Daniel Tomala <01159155@pw.edu.pl>
2022-01-22 14:48:30 +0100

    Resolved some problems with moving pawns phase And added some conditions to check if game is over

--- source/game_logic.py
+++ source/game_logic.py
@@ -9,15 +9,16 @@ from .interface import (OTHER_PLAYER, print_before_move, print_board,
                         print_choose_mode, print_empty_lines,
                         print_field_occupied, print_improper_id,
                         print_mill_occurred, print_no_pawn,
-                        print_not_your_pawn, print_pawns_left,
+                        print_not_your_pawn, print_pawns_left, print_possible_moves, print_remove_own_pawn,
                         print_starting_player,
                         print_transition_to_moving_phase, print_welcome)
 
-# Podwójny młynek
-# Koniec gry, gdy nie ma żadnego dostępnego ruchu
-# Koniec gry, gdy zostaną dwa
+# Podwójny młynek -DONE
+# Koniec gry, gdy nie ma żadnego dostępnego ruchu - DONE
+# Koniec gry, gdy zostaną dwa - DONE
 # Remis, gdy trzykrotnie powtórzy się sytuacja na planszy
 # Gdy graczowi zostaną trzy pionki może poruszać się swoimi pionkami na dowolne pola
+# Brak młynka przez 50 ruchów to remis
 
 
 def main():
@@ -42,8 +43,8 @@ def get_starting_player():
     number = random.randint(1, 2)
     return num_to_player[number]
 
-# Przenieść wszystkie printy do interfejsu
-# Dodać wyświetlanie pionków, które pozostały do umieszczenie i które zostały zbite
+# Przenieść wszystkie printy do interfejsu - DONE
+# Dodać wyświetlanie pionków, które pozostały do umieszczenie i które zostały zbite - DONE
 
 
 def setting_pawns_phase(board: Board) -> None:
@@ -51,10 +52,12 @@ def setting_pawns_phase(board: Board) -> None:
     first_player_pawns_no = board.pawns_number().value
     second_player_pawns_no = board.pawns_number().value
     while first_player_pawns_no > 0 or second_player_pawns_no > 0:
-        set_pawn_by_player(board, board.starting_player())
+        set_pawn_by_player(board, board.starting_player(),
+                           first_player_pawns_no)
         first_player_pawns_no -= 1
         print_board(board)
-        set_pawn_by_player(board, OTHER_PLAYER[board.starting_player()])
+        set_pawn_by_player(
+            board, OTHER_PLAYER[board.starting_player()], second_player_pawns_no)
         second_player_pawns_no -= 1
         print_board(board)
 
@@ -66,31 +69,79 @@ def moving_pawns_phase(board: Board):
         print_board(board)
         move_pawn_by_player(board, OTHER_PLAYER[board.starting_player()])
         print_board(board)
-
-# TODO
-
-
-def is_game_still_played(board: Board) -> bool:
-    return True
+    # TODO
+    check_winner()
 
 
-def set_pawn_by_player(board: Board, player: Player):
+def set_pawn_by_player(board: Board, player: Player, pawns_in_hand: int):
     print_before_move(player)
-    print_pawns_left(board, player)
+    print_pawns_left(board, player, pawns_in_hand)
     field = get_field_condition_field_is_free(
         board, "Where do you want to add your pawn: ")
     board.add_pawn(field, player)
-    player_with_mill = check_mill(board, field)
-    if player_with_mill is not None:
+    player_with_mill, mill_num = check_mill(board, field)
+    # if player_with_mill is not None:
+    #     print_board(board)
+    #     remove_opponents_pawn(board, player_with_mill)
+    if mill_num > 0:
+        print_mill_occurred(mill_num)
+    for _ in range(mill_num):
         print_board(board)
-        after_mill_occured(board, player_with_mill)
+        remove_opponents_pawn(board, player_with_mill)
     print_empty_lines(1)
 
 
+def move_pawn_by_player(board: Board, player: Player) -> None:
+    print_before_move(player)
+    # Podpowiedzi jakie na jakie pola można się ruszyć swoim pionkiem - DONE
+    while True:
+        curr_field = get_field_condition_proper_player(
+            board, player, "Which pawn do you want to move? Enter field id: ")
+
+        # Dodatkowo sprawdzić czy pola z którymi jest połączenie są zajęte - DONE
+        # Anulowanie wyboru danego pola - DONE
+        # Od razu blokowanie wykonania ruchu jeżeli nie ma żadnego wolnego pola dookoła -DONE
+        moves = possible_moves(board, curr_field, player)
+        if not moves:
+            print("There is no possible move with this pawn")
+            continue
+            # Coś, żeby wybrać jeszcze raz - DONE
+
+        print_possible_moves(moves)
+
+        new_field = get_field_condition_free_and_connection(
+            board, curr_field, "Where do you want to move your pawn? Enter field id: ")
+        if new_field is not None:
+            break
+
+    board.move_pawn(curr_field, new_field, player)
+    # Jeżeli nie został wykonany ruch no to nie sprawdza młynka - DONE
+    player_with_mill, mill_num = check_mill(board, new_field)
+    if mill_num > 0:
+        print_mill_occurred(mill_num)
+    for _ in range(mill_num):
+        print_board(board)
+        remove_opponents_pawn(board, player_with_mill)
+    print_empty_lines(1)
+
+
+def get_field_condition_free_and_connection(board: Board, curr_field: Field, message: str):
+    while True:
+        field = get_field_condition_field_is_free(board, message)
+        if field is None:
+            return None
+        if field.id() in curr_field.connections():
+            return field
+        else:
+            print("There is no connection beetween these fields")
+
+
 def get_field_condition_field_is_free(board: Board, message: str) -> Field:
-    # Czy tu nie będzie nieskończonej pętli kiedyś?
+    # Czy tu nie będzie nieskończonej pętli kiedyś? - RACZEJ NIE
     while True:
         id = get_user_input(message).upper()
+        if id == "`":
+            return None
         if id in FIELD_IDS:
             field = board.field_by_id(id)
             if board.is_field_free(field):
@@ -116,19 +167,25 @@ def get_field_condition_proper_player(board: Board, player: Player, message: str
             print_improper_id()
 
 
-def move_pawn_by_player(board: Board, player: Player) -> None:
-    print_before_move(player)
-    # Podpowiedzi jakie na jakie pola można się ruszyć swoim pionkiem
-    curr_field = get_field_condition_proper_player(
-        board, player, "Which pawn do you want to move? Enter field id: ")
-    new_field = get_field_condition_field_is_free(
-        board, "Where do you want to move your pawn? Enter field id: ")
-    board.move_pawn(curr_field, new_field, player)
-    player_with_mill = check_mill(board, new_field)
-    if player_with_mill is not None:
-        print_board(board)
-        after_mill_occured(board, player_with_mill)
-    print()
+def is_game_still_played(board: Board) -> bool:
+    if board.player_pawns_number(Player.FIRST) <= 2 or board.player_pawns_number(Player.SECOND) <= 2:
+        return False
+    # Koniec gry również gdy nie można wykonać żadnego ruchu
+    if not check_is_any_possible_move(board):
+        return False
+    # TODO
+    # if check_is_draw():
+    #     return False
+    return True
+
+
+def check_is_any_possible_move(board: Board):
+    for player in [Player.FIRST, Player.SECOND]:
+        for field in board.get_all_player_fields(player):
+            for connection in field.connections():
+                if board.field_by_id(connection).player() is None:
+                    return True
+    return False
 
 
 """
@@ -137,6 +194,7 @@ Checks if last move created a mill
 
 
 def check_mill(board: Board, field: Field):
+    mills_num = 0
     if field.coordiantes().position_top_middle_bottom() == pos.MIDDLE or field.coordiantes().position_left_center_right() == pos.CENTER:
         fields_to_check = []
         for square in pos_sq:
@@ -144,7 +202,8 @@ def check_mill(board: Board, field: Field):
             ).position_top_middle_bottom(), field.coordiantes().position_left_center_right())
             fields_to_check.append(found_field)
         if all([field.player() == check_field.player() for check_field in fields_to_check]):
-            return field.player()
+            # return field.player()
+            mills_num += 1
     if field.coordiantes().position_left_center_right() in [pos.LEFT, pos.RIGHT]:
         fields_to_check = []
         for position in [pos.TOP, pos.MIDDLE, pos.BOTTOM]:
@@ -152,7 +211,8 @@ def check_mill(board: Board, field: Field):
             ), position, field.coordiantes().position_left_center_right())
             fields_to_check.append(found_field)
         if all([field.player() == check_field.player() for check_field in fields_to_check]):
-            return field.player()
+            # return field.player()
+            mills_num += 1
     if field.coordiantes().position_top_middle_bottom() in [pos.TOP, pos.BOTTOM]:
         fields_to_check = []
         for position in [pos.LEFT, pos.CENTER, pos.RIGHT]:
@@ -160,11 +220,13 @@ def check_mill(board: Board, field: Field):
             ), field.coordiantes().position_top_middle_bottom(), position)
             fields_to_check.append(found_field)
         if all([field.player() == check_field.player() for check_field in fields_to_check]):
-            return field.player()
+            # return field.player()
+            mills_num += 1
+    return (field.player(), mills_num)
 
 
-def after_mill_occured(board: Board, player: Player):
-    print_mill_occurred()
+def remove_opponents_pawn(board: Board, player: Player):
+    # print_mill_occurred()
     while True:
         field_id = get_user_input(
             "Which pawn would you like to remove? Enter field id: ").upper()
@@ -172,12 +234,24 @@ def after_mill_occured(board: Board, player: Player):
             if board.field_by_id(field_id).player() is None:
                 print_no_pawn()
             elif board.field_by_id(field_id).player() == player:
-                print("You cannot remove your own pawn")
+                print_remove_own_pawn()
             else:
                 break
         else:
             print_improper_id()
     board.remove_pawn(board.field_by_id(field_id))
+
+
+def possible_moves(board: Board, field: Field, player: Player):
+    result = []
+    for conenction in field.connections():
+        if board.field_by_id(conenction).player() is None:
+            result.append(conenction)
+    return result
+
+
+def check_winner() -> Player:
+    pass
 
 
 def get_user_input(message):
